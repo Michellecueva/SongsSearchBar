7c540c8eba45dbb619f474a1ac1571e4fd272329
Michelle <Michelle.cueva@gmail.com>
2019-08-15 21:25:05 -0400

    Adss message when no result is found

--- SongsTableViewSearchBar/ViewController.swift
+++ SongsTableViewSearchBar/ViewController.swift
@@ -7,21 +7,113 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    let songs = Song.loveSongs
+    
+    
+    var songSearchResults: [Song] {
+        get {
+            guard let searchString = searchString else {
+                return songs
+            }
+            guard searchString != "" else {
+                return songs
+            }
+            
+            if let scopeTitles = songSearchBar.scopeButtonTitles {
+                let currentScopeIndex = songSearchBar.selectedScopeButtonIndex
+                switch scopeTitles[currentScopeIndex] {
+                case "Songs":
+                    return songs.filter({$0.name.lowercased().contains(searchString.lowercased())})
+                case "Artist":
+                    return songs.filter({$0.artist.lowercased().contains(searchString.lowercased())})
+                default:
+                    return songs
+                }
+            }
+            return songs 
+        }
+    }
+    
+    var searchString: String? = nil {
+        didSet {
+            self.songTableView.reloadData()
+        }
+    }
+    
+    @IBOutlet weak var songSearchBar: UISearchBar!
+    
+    @IBOutlet weak var songTableView: UITableView!
+    
 
+    @IBOutlet var resultNotFoundLabel: UILabel!
+    
+    func updateLabelFrame() {
+        resultNotFoundLabel.translatesAutoresizingMaskIntoConstraints = false
+        resultNotFoundLabel.centerYAnchor.constraint(equalTo: songTableView.centerYAnchor).isActive = true
+        resultNotFoundLabel.centerXAnchor.constraint(equalTo: songTableView.centerXAnchor).isActive = true
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        songTableView.dataSource = self
+        songTableView.delegate = self
+        songSearchBar.delegate = self
+        resultNotFoundLabel.isHidden = true
+//        updateLabelFrame()
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
-
+    
 
 }
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songSearchResults.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if songSearchResults.count == 0 {
+            resultNotFoundLabel.isHidden = false
+            songTableView.isHidden = true
+            return 0
+        }
+        resultNotFoundLabel.isHidden = true
+        songTableView.isHidden = false
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath)
+        let song = songSearchResults[indexPath.row]
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.artist
+        return cell
+    }
+    
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchString = searchBar.text
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+}
+
+
 
